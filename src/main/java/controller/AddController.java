2ea043afaa49026b60889ba1b6362bc0466f8b63
Mateusz Balcer <mateusz75319@gmail.com>
2019-03-13 21:26:30 +0100

    Correction of the view

--- src/main/java/controller/AddController.java
+++ src/main/java/controller/AddController.java
@@ -1,4 +1,15 @@
 package controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
 public class AddController {
+
+    @FXML
+    public void backToMainView(ActionEvent event) {
+    }
+
+    @FXML
+    public void addEmployee(ActionEvent event) {
+    }
 }

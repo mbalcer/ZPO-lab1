3f7dc6d0013dfb983ebd89c955cc4889fbb02e41
Mateusz Balcer <mateusz75319@gmail.com>
2019-03-13 22:16:11 +0100

    Created deleteView.fxml and his controller

---
+++ src/main/java/controller/DeleteController.java
@@ -0,0 +1,28 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+
+public class DeleteController {
+
+    @FXML
+    private ComboBox<?> cb_employee;
+
+    @FXML
+    private Button btn_delete;
+
+    @FXML
+    private Label lbl_info;
+
+    @FXML
+    public void deleteEmployee() {
+
+    }
+
+    @FXML
+    public void backToMainView() {
+    }
+}
